12400930395f35a89af44b8bcccb0842933bf2d4
krzysztofhanzelpolsl <154464969+krzysztofhanzelpolsl@users.noreply.github.com>
2025-08-21 14:39:01 +0200

    Update nginx.sh

--- traccar/rootfs/etc/cont-init.d/nginx.sh
+++ traccar/rootfs/etc/cont-init.d/nginx.sh
@@ -4,8 +4,8 @@
 # Configures NGINX for use with the Traccar server
 # ==============================================================================
 
-set -x              # pokaż każde polecenie przed wykonaniem
-exec 2>&1           # przekieruj stderr do stdout
+#set -x              # pokaż każde polecenie przed wykonaniem
+#exec 2>&1           # przekieruj stderr do stdout
 
 # Generate direct access configuration, if enabled.
 if bashio::var.has_value "$(bashio::addon.port 80)"; then
